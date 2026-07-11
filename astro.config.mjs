9cd2c59b413787575000e42bcd6fde4bdb854669
Vertedero <oigresedsus@gmail.com>
2026-07-11 08:20:11 -0600

    Publicado desde Obsidian

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import fg from "fast-glob";
 import fs from "node:fs";
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import remarkObsidianMd from 'remark-obsidian-md';
-import remarkMath from 'remark-math';
-import rehypeMathjax from 'rehype-mathjax';
-import remarkMathPreamble from './src/plugins/remark-math-preamble.mjs';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import remarkObsidianMd from "remark-obsidian-md";
+import remarkMath from "remark-math";
+import rehypeMathjax from "rehype-mathjax";
 
 const macroFiles = fg.sync("./src/math/macros/*.tex").sort();
 
@@ -26,37 +24,41 @@ for (const file of macroFiles) {
     macros[match[1]] = match[2];
   }
 }
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+
   markdown: {
     remarkPlugins: [
-      [remarkObsidianMd, { root: './src/content/garden' }], 
-      remarkMathPreamble,
-      remarkMath,      
+      [remarkObsidianMd, { root: "./src/content/garden" }],
+      remarkMath,
     ],
 
-rehypePlugins: [
-  [rehypeMathjax, {
-    tex: {
-      packages: {
-        "[+]": [
-          "ams",
-          "newcommand",
-          "configmacros",
-          "color",
-          "bbox",
-          "cancel",
-          "braket",
-          "enclose",
-          "textmacros",
-          "unicode"
-        ]
-      },
+    rehypePlugins: [
+      [
+        rehypeMathjax,
+        {
+          tex: {
+            packages: {
+              "[+]": [
+                "ams",
+                "newcommand",
+                "configmacros",
+                "color",
+                "bbox",
+                "cancel",
+                "braket",
+                "enclose",
+                "textmacros",
+                "unicode",
+              ],
+            },
 
-  macros,
-    }
-  }],
-],
+            macros,
+          },
+        },
+      ],
+    ],
   },
 });
